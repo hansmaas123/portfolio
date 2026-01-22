183caeb616f53cdb583dcae2eff8859bb91d94e4
hansmaas123 <>
2026-01-22 01:09:09 +0100

    small bugfix

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,8 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: "/",
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          "react-vendor": ["react", "react-dom", "react-router-dom"],
+          gsap: ["gsap"],
+          "framer-motion": ["framer-motion"],
+        },
+      },
+    },
+  },
+});
